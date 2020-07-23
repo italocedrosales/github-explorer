96c6f7d9bec7f2bbca12d2a74adc04634d38448c
Italo Cedro <italocedro@pop-os.localdomain>
2020-07-23 16:00:26 -0300

    Salvando no storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -19,7 +19,38 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  // async function findRepoDefault() {
+  //   const response = await api.get<Repository>(
+  //     `repos/italocedrosales/github-explorer`,
+  //   );
+
+  //   const repository = response.data;
+
+  //   setRepositories([...repositories, repository]);
+  // }
+
+  // useEffect(() => {
+  //   findRepoDefault();
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
